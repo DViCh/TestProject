fa2ff671d4e9ddb0c2108ac8c33aacac9dc41268
Derick <Dalmightyone@gmail.com>
2022-06-30 14:59:01 +1000

    PHPUnit Setup + Test1

---
+++ T1/LetterRepeatClass.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * Exercise 1 - Words without any repeating letters
+
+
+Create a function that returns TRUE if a word does not have any repeating letter.
+
+Examples of valid words:
+
+documentarily
+aftershock
+countryside
+six-year-old
+
+
+Examples of invalid words:
+
+Double-down
+Euclidean
+epidemic
+
+
+The function must be in a dedicated class and must be fully unit tested.
+
+Tests
+Input must be string
+Valid Output - Return TRUE if correct input
+Incorrect Output - Return FALSE if incorrect input
+
+Convert to lowerspace, remove all non-alphabetical characters.
+insert into associative array key value pair of [char => boolean].
+If key value pair exists for letters, invalid word.
+
+PHP returns 1 if TRUE, empty if FALSE
+
+*/
+
+namespace Exercise\E1;
+
+class LetterRepeatClass
+{
+    public function LetterRepeat(string $input): bool
+    {
+        $word = (strtolower($input));
+        $chararray = str_split(preg_replace('/[^a-z]/','',$word));
+        $array = array();
+        foreach ($chararray as $char) {
+            if (isset($array[$char])) {
+                return FALSE;
+            }
+            else {
+                $array[$char] = TRUE;
+            }
+        }
+        return TRUE;
+    }
+}

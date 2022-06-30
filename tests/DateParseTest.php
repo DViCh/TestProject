282fb8563f358e2757622a98dfcfd47cc1c803d5
DD <DC.DerickChan@gmail.com>
2022-06-30 17:25:05 +1000

    T2 + a few tests

---
+++ tests/DateParseTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\DateParseClass;
+
+require 'app/T2/DateParseClass.php';
+
+class DateParseTest extends PHPUnit\Framework\TestCase
+{
+
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testNoInput() // Case with no input - ArgumentCountError.
+    {
+        $DateParse = new DateParseClass();
+        $this->expectException(ArgumentCountError::class);
+        $output = $DateParse->DateParse();
+    }
+
+    public function testIncorrectOutput() // Is null if strtotime returns false.
+    {
+        $DateParse = new DateParseClass();
+        $output = $DateParse->DateParse("123");
+        $this->assertnull($output);
+    }
+
+    public function testCorrectOutput() // Assert correct output is string from strtotime() output;
+    {
+        $DateParse = new DateParseClass();
+        $output = $DateParse->DateParse("Last Monday of October 2021");
+        $this->assertIsString($output);
+    }
+
+
+}
